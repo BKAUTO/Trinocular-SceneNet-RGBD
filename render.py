9c24da43a61cd7d66df2d9c14d0a7c46b801c491
Weihao Yuan <whyuan111@gmail.com>
2019-09-01 17:11:22 +0800

    9-1

--- render.py
+++ render.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 import os
 
-for i in range(200, 1000):
+for i in range(320, 1000):
 	val = os.popen('cd /home/will/WBK/SceneNet/dysonroboticslab-scenenetrgb-d-afb686496a4a/renderer/build;mkdir /home/will/WBK/render_out_'+str(i)+'/;\
 	echo "yuanweihao" | sudo -S ./scenenet_render /home/will/Backup/ShapeNetCore.v2/ /home/will/WBK/SceneNet/SceneNetRGBD_Layouts/ /home/will/WBK/SceneNet/dysonroboticslab-scenenetrgb-d-afb686496a4a/camera_trajectory_generator/build/scene_and_trajectory_description.txt /home/will/WBK/render_out_'+str(i)+'/').read()
 	if ("Average intensity too extreme:" in val or "CVD::Exceptions" in val):
